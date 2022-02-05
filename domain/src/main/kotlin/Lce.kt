326922ebf5513c7d3231deb6da3924cffc16f285
larry.p <larry.p@kakaocorp.com>
2022-02-05 19:35:50 +0900

    응답 전달을 위한 Loading, Content, Error 클래스 추가

---
+++ domain/src/main/kotlin/Lce.kt
@@ -0,0 +1,5 @@
+sealed class Lce<out T> {
+    object Loading : Lce<Nothing>() // 1
+    data class Content<T>(val data: T) : Lce<T>() // 2
+    data class Error(val error: Throwable) : Lce<Nothing>() // 3
+}
